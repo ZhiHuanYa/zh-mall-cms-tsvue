2757777fdb4a2541eabd7dc2263bedc1d394979d
zhihuan <wcg17732124847@outlook.com>
2022-10-07 23:40:09 +0800

    fix(util - mapmenus): add firstRoute

--- src/utils/map-menus.ts
+++ src/utils/map-menus.ts
@@ -2,29 +2,31 @@ import { IBreadcrumb } from '@/base-ui/breadcrumb'
 import { RouteRecordRaw } from 'vue-router'
 
 let firstMenu: any = ''
+let firstRoute: RouteRecordRaw | undefined = undefined
 
 export function mapMenusToRoutes(userMenus: any[]): RouteRecordRaw[] {
   const routes: RouteRecordRaw[] = []
 
   // 1. 先去加载默认所有的 routes
-  const allRoutes: RouteRecordRaw[] = []
+  const localRoutes: RouteRecordRaw[] = []
   const routeFiles = require.context('../router/main', true, /\.ts$/)
   routeFiles.keys().forEach((key) => {
     const route = require('../router/main' + key.split('.')[1])
-    allRoutes.push(route.default)
+    localRoutes.push(route.default)
   })
 
   // 2. 根据菜单获取需要添加的 routes
   const _recurseGetRoute = (menus: any[]) => {
     for (const menu of menus) {
       if (menu.type === 2) {
-        const route = allRoutes.find((route) => route.path === menu.url)
+        const route = localRoutes.find((route) => route.path === menu.url)
         if (route) routes.push(route)
-        if (!firstMenu) {
+        if (!firstRoute && !firstMenu) {
           firstMenu = menu
+          firstRoute = route
         }
       } else {
-        _recurseGetRoute(menu.children)
+        _recurseGetRoute(menu.children ?? [])
       }
     }
   }
@@ -32,6 +34,34 @@ export function mapMenusToRoutes(userMenus: any[]): RouteRecordRaw[] {
   _recurseGetRoute(userMenus)
 
   return routes
+}
+
+export function mapPathToBreadpaths(currentPath: string, userMenus: any[]) {
+  const breadPaths: IBreadcrumb[] = []
+
+  const _recurseGetPath = (menus: any[]): any => {
+    for (const menu of menus) {
+      if (menu.type === 1) {
+        const foundMenu = _recurseGetPath(menu.children)
+        if (foundMenu) {
+          breadPaths.push({ name: menu.name, path: menu.url })
+        }
+      } else if (menu.type === 2 && menu.url === currentPath) {
+        breadPaths.push({ name: menu.name, path: menu.url })
+        return menu
+      }
+    }
+  }
+
+  _recurseGetPath(userMenus)
+
+  return breadPaths.reverse()
+}
+
+export function pathMapBreadcrumbs(userMenus: any[], currentPath: string) {
+  const breadcrumbs: IBreadcrumb[] = []
+  pathMapToMenu(userMenus, currentPath, breadcrumbs)
+  return breadcrumbs
 }
 
 export function pathMapToMenu(
@@ -43,20 +73,14 @@ export function pathMapToMenu(
     if (menu.type === 1) {
       const findMenu = pathMapToMenu(menu.children ?? [], currentPath)
       if (findMenu) {
-        breadcrumbs?.push({ name: menu.name })
-        breadcrumbs?.push({ name: findMenu.name })
+        breadcrumbs?.push({ name: menu.name, path: '/' })
+        breadcrumbs?.push({ name: findMenu.name, path: '/' })
         return findMenu
       }
     } else if (menu.type === 2 && menu.url === currentPath) {
       return menu
     }
   }
-}
-
-export function pathMapBreadcrumbs(userMenus: any[], currentPath: string) {
-  const breadcrumbs: IBreadcrumb[] = []
-  pathMapToMenu(userMenus, currentPath, breadcrumbs)
-  return breadcrumbs
 }
 
 export function mapMenusToPermissions(userMenus: any[]) {
@@ -93,4 +117,4 @@ export function MenuMapLeafKeys(menuList: any[]) {
   return leftKeys
 }
 
-export { firstMenu }
+export { firstMenu, firstRoute }
